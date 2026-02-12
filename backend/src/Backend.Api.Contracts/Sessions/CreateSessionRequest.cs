b0412b319f69d491be6748f2205a5627c55665d8
Bahrami <m.m.bhrm85@gmail.com>
2026-02-12 13:40:40 -0800

    Making backend thingy ready.

--- backend/src/Backend.Api.Contracts/Sessions/CreateSessionRequest.cs
+++
@@ -1,5 +0,0 @@
-namespace Backend.Api.Contracts.Sessions;
-
-public record CreateSessionRequest {
-    public required string Passcode { get; init; }
-}
--- backend/src/Backend.Api/Sessions/Validators/CreateSessionRequestValidator.cs
+++
@@ -1,6 +0,0 @@
-using Backend.Api.Contracts.Sessions;
-using FluentValidation;
-
-namespace Backend.Api.Sessions.Validators;
-
-public class CreateSessionRequestValidator : AbstractValidator<CreateSessionRequest>;
