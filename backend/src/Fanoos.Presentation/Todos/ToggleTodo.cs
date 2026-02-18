e0b1b55382515ea168abf6df5195d8dfdcfa1f0b
Bahrami <m.m.bhrm85@gmail.com>
2026-02-18 14:26:39 -0800

    Fix a couple of issues.

--- backend/src/Fanoos.Presentation/Todos/ToggleTodo.cs
+++ backend/src/Fanoos.Presentation/Todos/ToggleTodo.cs
@@ -25,9 +25,9 @@ internal sealed class ToggleTodo : IEndpoint {
     }
 
     private static async Task<IResult> Handle(
-        [FromServices] ISender                       mediator,
-        [FromRoute]    Guid                          id,
-        [FromBody]     ToggleTodoRequest             request
+        [FromServices] ISender            mediator,
+        [FromRoute]    Guid               id,
+        [FromBody]     ToggleTodoRequest? request=null
     ) {
         ErrorOr<Todo> result = await mediator.Send(MapToCommand(request, id));
         if (result.IsError) return result.Errors.ToProblem();
@@ -36,14 +36,14 @@ internal sealed class ToggleTodo : IEndpoint {
         return Results.Ok(todo.MapToResponse());
     }
 
-    private static ToggleTodoCommand MapToCommand(ToggleTodoRequest request, Guid id) {
+    private static ToggleTodoCommand MapToCommand(ToggleTodoRequest? request, Guid id) {
         return new ToggleTodoCommand {
             Id       = id,
-            IsDone   = request.IsDone,
+            IsDone   = request?.IsDone,
         };
     }
 
     internal sealed record ToggleTodoRequest {
-        public required bool? IsDone { get; init; }
+        public bool? IsDone { get; init; }
     }
 }
